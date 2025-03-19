536646f5002f8caf879004fcd138c452651c18c0
Chintan Patel <chintanpatelit024@gmail.com>
2025-03-19 09:59:51 +0530

    Added: Place the Mines!

--- Array-Minesweeper/source/GameLoop/Gameplay/Cell.cpp
+++ Array-Minesweeper/source/GameLoop/Gameplay/Cell.cpp
@@ -10,6 +10,7 @@ namespace Gameplay
         //Get cell position
         sf::Vector2f cellScreenPosition = getCellScreenPosition(width, height);
         cell_button = new Button(cell_texture_path, cellScreenPosition, width * slice_count, height);
+        current_cell_state = CellState::OPEN; // Temporarily set to OPEN for visualization
     }
 
     void Cell::render(sf::RenderWindow& window)
